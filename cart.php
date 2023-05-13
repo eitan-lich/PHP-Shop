6476fa290b48787c2d0dd93d7a4764f41027b179
eitan <eitanlich2000@gmail.com>
2023-05-13 17:34:51 +0300

    Fixed cart bug and adding modal

--- cart.php
+++ cart.php
@@ -6,17 +6,18 @@ $con = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DB);
 if (isset($_SESSION['cart'], $_POST['item_id'])) {
     $item_to_remove = $_POST['item_id'];
     unset($_SESSION['cart'][array_search($item_to_remove, $_SESSION['cart'])]);
-} 
+}
 
-if(isset($_POST['clear_cart'])) {
-   $_SESSION['cart'] = array();
+if (isset($_POST['clear_cart'])) {
+    $_SESSION['cart'] = array();
 }
 
 
 ?>
 
-    <?php require "header.php"?>
-    <body>
+<?php require "header.php" ?>
+
+<body>
     <main id='cart-main'>
         <?php
         if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
@@ -36,11 +37,31 @@ if(isset($_POST['clear_cart'])) {
             }
             echo "
             <button class='checkout-btn'>Continue to checkout</button>
-            <button class='clear-cart-btn'>Clear cart</button>";
+            <button class='clear-cart-btn'>Clear cart</button>
+            <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#test'>Click here</button>
+            <div class='modal fade' id='test' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+            <div class='modal-dialog' role='document'>
+                <div class='modal-content'>
+                <div class='modal-header'>
+                    <h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>
+                    <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+                </div>
+                <div class='modal-body'>
+                    ...
+                </div>
+                <div class='modal-footer'>
+                    <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                    <button type='button' class='btn btn-primary'>Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>";
         }
         ?>
     </main>
     <script src="js/script.js"></script>
-    </body>
+</body>
 
 </html>
--- js/script.js
+++ js/script.js
@@ -1,50 +1,62 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Make all item descriptions hidden at first
+  $(".item-description").hide();
 
-// Make all item descriptions hidden at first
-    $(".item-description").hide();
+  // Add an event listener to all buttons to show more info, then only the specific button we requested will show its item description
+  $(".see-more-btn").click(function () {
+    $(this).siblings("p.item-description").toggle();
+  });
 
-// Add an event listener to all buttons to show more info, then only the specific button we requested will show its item description
-    $(".see-more-btn").click(function() {
-        $(this).siblings("p.item-description").toggle();    
-    });
-
-
-// Adding items to cart
-    $(".add-cart-btn").click(function() {
-        let num = $(this).siblings("[name='item_id']").val()
-        $.post("index.php", {
-            item_id : num.substring(1)
-        }, function() {
-            $.getJSON("count.php", function(data, response) {
-                $("#cart-number").html(data.count);
-            })
-        })
-    });
-                
-
-// Removing item from cart and update the cart count 
-
-    $(".remove-btn").click(function() {
-        let num = $(this).siblings("[name='item_id']").val()
-        var el = $(this);
-        $.post("cart.php", {
-            item_id : num.substring(1)
-        }, function() {
-          $(el).parent().remove();
-          $.getJSON("count.php", function(data, response) {
-            $("#cart-number").html(data.count);
-        })
+  // Adding items to cart
+  $(".add-cart-btn").click(function () {
+    let num = $(this).siblings("[name='item_id']").val();
+    $.post(
+      "index.php",
+      {
+        item_id: num.substring(1),
+      },
+      function () {
+        $.getJSON("count.php", function (data, response) {
+          $("#cart-number").html(data.count);
         });
-    })
+      }
+    );
+  });
 
-// Clear cart button deleting all items from cart
+  // Removing item from cart and update the cart count
 
-    $(".clear-cart-btn").click(function() {
-        $("[name='item_id']").parent().remove();
-        $(".checkout-btn, .clear-cart-btn").remove();
-        $.post("cart.php", {
-            clear_cart : true
-        })
-    })
+  $(".remove-btn").click(function () {
+    let num = $(this).siblings("[name='item_id']").val();
+    var el = $(this);
+    $.post(
+      "cart.php",
+      {
+        item_id: num.substring(1),
+      },
+      function () {
+        $(el).parent().remove();
+        $.getJSON("count.php", function (data, response) {
+          $("#cart-number").html(data.count);
+        });
+      }
+    );
+  });
 
+  // Clear cart button deleting all items from cart
+
+  $(".clear-cart-btn").click(function () {
+    $("[name='item_id']").parent().remove();
+    $(".checkout-btn, .clear-cart-btn").remove();
+    $.post(
+      "cart.php",
+      {
+        clear_cart: true,
+      },
+      function () {
+        $.getJSON("count.php", function (data, response) {
+          $("#cart-number").html(data.count);
+        });
+      }
+    );
+  });
 });
